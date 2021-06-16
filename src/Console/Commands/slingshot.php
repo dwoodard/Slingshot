e28ab8dea142b1eaeb801e43bd24631dace041ca
Dustin Woodard <dustin.woodard@gmail.com>
2021-06-16 13:10:01 -0600

    slash

--- src/Console/Commands/slingshot.php
+++ src/Console/Commands/slingshot.php
@@ -216,7 +216,7 @@ class slingshot extends Command
 
             $filepath = resource_path('views/layouts/app.blade.php');
             if (!file_exists($filepath)) {
-                copy(__DIR__ . '../../stubs/resources/views/layouts/app.blade.php', $filepath);
+                copy(__DIR__ . '/../../stubs/resources/views/layouts/app.blade.php', $filepath);
                 chmod($filepath, 0777);
                 $this->info("- $filepath created");
             } else {
