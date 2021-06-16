061646ba858b7fb63e6bc07d5caf4917be91db80
Dustin Woodard <dustin.woodard@gmail.com>
2021-06-16 13:08:21 -0600

    dir path

--- src/Console/Commands/slingshot.php
+++ src/Console/Commands/slingshot.php
@@ -215,9 +215,8 @@ class slingshot extends Command
             $this->info('installed inertia:middleware - app/Http/Middleware/HandleInertiaRequests.php ');
 
             $filepath = resource_path('views/layouts/app.blade.php');
-            dd($filepath);
             if (!file_exists($filepath)) {
-                copy('../../stubs/resources/views/layouts/app.blade.php', $filepath);
+                copy(__DIR__ . '../../stubs/resources/views/layouts/app.blade.php', $filepath);
                 chmod($filepath, 0777);
                 $this->info("- $filepath created");
             } else {
