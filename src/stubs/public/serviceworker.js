671ec276757770b3f4ca51d90fc79bcef8169421
Dustin Woodard <dustin.woodard@gmail.com>
2021-06-17 15:30:45 -0600

    sw.js

--- src/stubs/public/serviceworker.js
+++ src/stubs/public/serviceworker.js
@@ -14,39 +14,39 @@ var filesToCache = [
 ];
 
 // Cache on install
-self.addEventListener("install", event => {
-    this.skipWaiting();
-    event.waitUntil(
-        caches.open(staticCacheName)
-            .then(cache => {
-              return cache ? cache.addAll(filesToCache) : null;
-            })
-    )
-});
+// self.addEventListener("install", event => {
+//     this.skipWaiting();
+//     event.waitUntil(
+//         caches.open(staticCacheName)
+//             .then(cache => {
+//               return cache ? cache.addAll(filesToCache) : null;
+//             })
+//     )
+// });
 
 // Clear cache on activate
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames
-                    .filter(cacheName => (cacheName.startsWith("pwa-")))
-                    .filter(cacheName => (cacheName !== staticCacheName))
-                    .map(cacheName => caches.delete(cacheName))
-            );
-        })
-    );
-});
+// self.addEventListener('activate', event => {
+//     event.waitUntil(
+//         caches.keys().then(cacheNames => {
+//             return Promise.all(
+//                 cacheNames
+//                     .filter(cacheName => (cacheName.startsWith("pwa-")))
+//                     .filter(cacheName => (cacheName !== staticCacheName))
+//                     .map(cacheName => caches.delete(cacheName))
+//             );
+//         })
+//     );
+// });
 
 // Serve from Cache
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-            .catch(() => {
-                return caches.match('offline');
-            })
-    )
-});
+// self.addEventListener("fetch", event => {
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then(response => {
+//                 return response || fetch(event.request);
+//             })
+//             .catch(() => {
+//                 return caches.match('offline');
+//             })
+//     )
+// });
