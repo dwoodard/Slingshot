52ecc6f9726f694dd148ad0a44b6d05676fcdbe7
Dustin Woodard <dustin.woodard@gmail.com>
2021-06-28 14:19:14 -0600

    slingshot

---
+++ src/stubs/app/Http/Controllers/PagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use Inertia\Response;
+
+class PagesController extends Controller
+{
+    public function dashboard(): Response
+    {
+        /**
+         * @get('/dashboard')
+         * @name('dashboard')
+         * @middlewares(web, auth:sanctum)
+         */
+
+        $data = [
+            'foo'=>'bar'
+        ];
+        return Inertia::render('dashboard', $data);
+    }
+}
