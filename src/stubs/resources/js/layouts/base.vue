684ebc7946159937d2e7ef14de959863b6102924
Dustin Woodard <dustin.woodard@gmail.com>
2021-06-28 10:29:44 -0600

    base.vue

--- src/stubs/resources/js/layouts/base.vue
+++ src/stubs/resources/js/layouts/base.vue
@@ -1,27 +1,32 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer
-      v-model="drawer"
-      app>
+        v-model="sidebarDrawer"
+        app>
       <!--  -->
     </v-navigation-drawer>
 
     <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"/>
+      <v-app-bar-nav-icon @click="sidebarDrawer = !sidebarDrawer"/>
 
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <slot></slot>
     </v-main>
   </v-app>
 </template>
 
 <script>
-  export default {
-
-  };
+export default {
+  data() {
+    return {
+      sidebarDrawer: null,
+      bottomDrawer: null
+    };
+  },
+};
 </script>
 
 <style scoped>
