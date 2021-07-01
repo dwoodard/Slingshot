54115f36ed61915fd3b46a8d3af91d070b59abd6
Dustin Woodard <dustin.woodard@gmail.com>
2021-07-01 12:18:45 -0600

    slingshot

--- src/stubs/resources/js/pages/settings/index.vue
+++ src/stubs/resources/js/pages/settings/index.vue
@@ -1,18 +1,15 @@
 <template>
-  <app-layout hide-breadcrumbs>
-    <div>
-      <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-        Settings
-      </div>
+  <div>
+    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+      Settings
     </div>
-  </app-layout>
+  </div>
+
 </template>
 
 <script>
-  import Layout from '@/layouts/Layout';
-
   export default {
-    layout: Layout
+
   };
 </script>
 
