541466e72ff5a946e3cf6b7bc90d7acbccd21d1a
Dustin Woodard <dustin.woodard@gmail.com>
2021-07-05 13:13:53 -0600

    slingshot

---
+++ src/stubs/app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class AdminController extends Controller
+{
+    public function dashboard(): Response
+    {
+        $data = [];
+
+        return Inertia::render('Admin/Dashboard', $data);
+    }
+}
---
+++ src/stubs/resources/js/pages/Admin/Dashboard/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid>
+    Dashboard
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+</style>
