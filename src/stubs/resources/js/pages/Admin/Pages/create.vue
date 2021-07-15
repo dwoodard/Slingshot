f496574240147310768b572307ecfabda5158c86
Dustin Woodard <dustin.woodard@gmail.com>
2021-07-15 16:48:41 -0600

    slingshot

---
+++ src/stubs/resources/js/pages/Admin/Pages/create.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-dialog v-model="show" max-width="500px" class="pa-3">
+    <v-card>
+      <form @submit.prevent="submit">
+        <v-card-text>
+          <v-text-field
+            v-model="form.title"
+            :error-messages="form.errors.title"
+            label="Title"
+            required/>
+          <v-text-field
+            v-model="form.slug"
+            :error-messages="form.errors.slug"
+            label="Slug"
+            required/>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn color="primary" text @click.stop="show=false">Close</v-btn>
+          <v-spacer/>
+          <v-btn type="submit" color="primary">Create</v-btn>
+        </v-card-actions>
+      </form>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+  import {toKebabCase} from '@/helper';
+
+  export default {
+    props: {
+      value: Boolean
+    },
+    data() {
+      return {
+        form: this.$inertia.form({
+          title: '',
+          slug: ''
+        })
+      };
+    },
+    computed: {
+      show: {
+        get() {
+          return this.value;
+        },
+        set(value) {
+          this.$emit('input', value);
+        }
+      },
+      title() {
+        return this.form.title;
+      }
+    },
+    methods: {
+      toKebabCase,
+      submit() {
+        this.form.post(route('admin.page.create'), {
+          onSuccess: (data) => {
+            this.show = false;
+          },
+          onFinish: () => this.form.reset()
+        });
+      }
+    },
+    watch: {
+      title() {
+        this.form.slug = toKebabCase(this.form.title)
+          .replace('.', '');
+      }
+    }
+  };
+</script>
--- src/stubs/resources/js/pages/Admin/Pages/index.vue
+++ src/stubs/resources/js/pages/Admin/Pages/index.vue
@@ -58,41 +58,41 @@
         </inertia-link>
         <v-icon
           small
-          @click="deleteItem()">
+          @click="deleteItem(item)">
           mdi-delete
         </v-icon>
-
-        <v-dialog
-          v-model="showDeleteConfirm"
-          max-width="290">
-          <v-card>
-            <v-card-title class="text-h5">
-              Delete Page?
-            </v-card-title>
-
-            <v-card-text>
-              Are you sure you and to delete this page?
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer/>
-
-              <v-btn
-                text
-                @click="showDeleteConfirm = false">
-                Disagree
-              </v-btn>
-
-              <v-btn
-                color="red darken-1 white--text"
-                @click="deleteItemConfirm(item);">
-                Delete
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
       </template>
     </v-data-table>
+
+    <portal to="modals">
+      <v-dialog
+        v-model="showDeleteConfirm"
+        max-width="500">
+        <v-card>
+          <v-card-title class="text-h5">
+            Delete Page?
+          </v-card-title>
+
+          <v-card-text>
+            Are you sure you and to delete this page?
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              text
+              @click="showDeleteConfirm = false">
+              Don't Delete
+            </v-btn>
+            <v-spacer/>
+            <v-btn
+              color="red darken-1 white--text"
+              @click="deleteItemConfirm();">
+              Delete
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </portal>
   </v-container>
 </template>
 
@@ -133,19 +133,21 @@
             value: 'actions',
             sortable: false
           }
-        ]
+        ],
+        selectedPage: null
       };
     },
     methods: {
-      deleteItem() {
+      deleteItem(item) {
+        this.selectedPage = item;
         this.showDeleteConfirm = true;
       },
 
       deleteItemConfirm(item) {
-        this.$inertia.delete(route('admin.page.delete', item), {
+        this.$inertia.delete(route('admin.page.delete', this.selectedPage), {
           onSuccess: (data) => {
-            this.editedIndex = this.pages.indexOf(item);
-            this.pages.splice(this.editedIndex, 1);
+            // this.editedIndex = this.pages.indexOf(item);
+            // this.pages.splice(this.editedIndex, 1);
             this.showDeleteConfirm = false;
           },
 
